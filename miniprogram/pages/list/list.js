59996275ac52372df15ccab16d997fc891da2e38
mogq <3385532575@qq.com>
2019-12-25 11:57:26 +0800

    添加商品组件，商品详情页

--- miniprogram/pages/list/list.js
+++ miniprogram/pages/list/list.js
@@ -3,8 +3,43 @@ const app = getApp()
 
 Page({
   data: {
+    list: []
   },
 
-  onLoad: function () {
+  // 加载列表
+  onLoad: function() {
+    this.getData()
   },
+
+  // 获取数据
+  getData: function() {
+    let data = []
+    for (let i = 0; i < 10; i++) {
+      data.push({
+        id: i,
+        image: "/images/photo.png",
+        title: "名称",
+        type: "类型",
+        detail: "描述: 的说法是否has都是繁华撒地方哈师大"
+      })
+    }
+    this.data.list = data.concat(this.data.list);
+    this.setData({
+      'list': this.data.list
+    });
+  },
+
+  // 加载下一页
+  onReachBottom: function() {
+    console.log('可以分页了')
+    this.getData()
+  },
+
+  // 查看详情
+  goDetail: function(event) {
+    console.log('查看详情', event.detail)
+    wx.navigateTo({
+      url: `/pages/detail/detail?id=${event.detail.id}`,
+    })
+  }
 })
